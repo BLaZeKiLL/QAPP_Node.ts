a2195e3e1b2777878f906a6926d590548a267069
BLaZeKiLL <laldevashish@gmail.com>
2019-01-22 11:51:06 +0530

    date handeling 2

--- src/Modules/scheduler.ts
+++ src/Modules/scheduler.ts
@@ -8,7 +8,8 @@ import { Firebase } from './firebase';
 class Scheduler {
 
   public static schedule(quizID: Schema.Types.ObjectId, date: Date): void {
-    new cron.CronJob(date, async () => {
+    Log.main.info(`QUIZ SCHEDULED FOR ${date.toISOString()}`);
+    new cron.CronJob(date.toISOString(), async () => {
       try {
         const quiz = await Quiz.getOne(undefined, quizID);
         quiz.targets.forEach((target: string) => {
@@ -19,7 +20,6 @@ class Scheduler {
 
       }
     }, undefined, true, 'Asia/Kolkata');
-    Log.main.info(`QUIZ SCHEDULED FOR ${date}`);
   }
 
 }
