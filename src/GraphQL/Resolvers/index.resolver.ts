455bb24a3cc377942bc039303bcc787e6fb71a61
BLaZeKiLL <laldevashish@gmail.com>
2019-02-14 16:16:45 +0530

    index resolver updated

--- src/GraphQL/Resolvers/index.resolver.ts
+++ src/GraphQL/Resolvers/index.resolver.ts
@@ -1,5 +1,4 @@
 import { Query as adminQuery, Mutation as adminMutation } from './admin.resolver';
-import { Query as courseQuery, Mutation as courseMutation } from './course.resolver';
 import { Query as miscQuery, Mutation as miscMutation } from './misc.resolver';
 import { Query as questionQuery, Mutation as questionMutation } from './question.resolver';
 import { Query as quizQuery, Mutation as quizMutation } from './quiz.resolver';
@@ -10,7 +9,6 @@ import { Query as teacherQuery, Mutation as teacherMutation } from './teacher.re
 export const Resolvers = {
   // Query
     ...adminQuery,
-    ...courseQuery,
     ...miscQuery,
     ...questionQuery,
     ...quizQuery,
@@ -19,7 +17,6 @@ export const Resolvers = {
     ...teacherQuery,
   // Mutation
     ...adminMutation,
-    ...courseMutation,
     ...miscMutation,
     ...questionMutation,
     ...quizMutation,
