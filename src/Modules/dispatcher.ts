7340a8dbdbde9f263f85dabac69eb288b83d8b17
BLaZeKiLL <laldevashish@gmail.com>
2019-02-28 01:19:29 +0530

    Quiz cache created
    addition and retrival methods created for new quiz cache

--- src/Modules/dispatcher.ts
+++ src/Modules/dispatcher.ts
@@ -10,6 +10,9 @@ class Dispatcher {
 
   private static quizes: Map<string, {_id: string, JSON: string}> = new Map<string, {_id: string, JSON: string}>();
 
+  private static stu_email_qid_map: Map<string, string> = new Map<string, string>();
+  private static qid_qjson_map: Map<string, string> = new Map<string, string>();
+
   public static distribute(target: string, quiz: IQuiz): void {
     Log.main.info('QUIZ READY FOR DISPATCHING: ' + target);
     this.quizes.set(target, { _id: quiz._id.toString(), JSON: JSON.stringify(quiz) });
@@ -23,6 +26,32 @@ class Dispatcher {
       Log.main.info('QUIZ DISPATCHED');
       return quiz;
     }
+  }
+
+  public static getFromCache(email: string): {_id: string, JSON: string} {
+    const id = this.stu_email_qid_map.get(email);
+    const json = this.qid_qjson_map.get(id);
+    if (json === undefined || json === null || id === undefined || id ===  null) {
+      throw new Error('Quiz Not Found');
+    } else {
+      Log.main.info('QUIZ DISPATCHED');
+      return {
+        _id: id,
+        JSON: json
+      };
+    }
+  }
+
+  public static cache(quiz: IQuiz): void {
+    quiz.targetEmails.forEach(email => {
+      this.stu_email_qid_map.set(email, quiz._id.toString())
+    });
+    const light_quiz: IQuiz = Object.create(quiz);
+    light_quiz.results = undefined;
+    light_quiz.targetEmails = undefined;
+    Log.main.info(`QUIZ: ${JSON.stringify(quiz)}`);
+    Log.main.info(`LIGHT QUIZ: ${JSON.stringify(light_quiz)}`);
+    this.qid_qjson_map.set(quiz._id.toString(), JSON.stringify(light_quiz));
   }
 
   public static clear(): void {
